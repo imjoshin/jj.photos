6aaa3af1ed350728ea96827400096018dae59910
Josh Johnson <jcjohnson77@gmail.com>
2022-01-03 21:58:57 -0500

    Initial setup with css theming

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+const defaultTheme = {
+    text: '#000',
+    background: '#fff',
+}
+
+export const Theme = {
+    light: {
+        ...defaultTheme,
+    },
+    dark: {
+        ...defaultTheme,
+        text: '#fff',
+        background: '#333842',
+    }
+}

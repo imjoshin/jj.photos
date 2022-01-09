c20cca7d300077fa0d0f3210be14d12db3d82677
Josh Johnson <jcjohnson77@gmail.com>
2022-01-09 11:40:38 -0500

    Erase previous content

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,22 +2,14 @@
 import { Box, Flex, Link, useColorMode, jsx } from "theme-ui"
 
 const Footer = () => {
-  const [colorMode, setColorMode] = useColorMode()
-  const isDark = colorMode === `dark`
-  const toggleColorMode = (e: any) => {
-    setColorMode(isDark ? `light` : `dark`)
-  }
+  // const [colorMode, setColorMode] = useColorMode()
+  // const isDark = colorMode === `dark`
+  // const toggleColorMode = (e: any) => {
+  //   setColorMode(isDark ? `light` : `dark`)
+  // }
 
   return (
     <Box as="footer" variant="footer">
-      <button
-        sx={{ variant: `buttons.toggle`, fontWeight: `semibold`, display: `block`, mx: `auto`, mb: 3 }}
-        onClick={toggleColorMode}
-        type="button"
-        aria-label="Toggle dark mode"
-      >
-        {isDark ? `Light` : `Dark`}
-      </button>
       Copyright &copy; {new Date().getFullYear()}. All rights reserved.
       <br />
       <Flex
@@ -30,24 +22,21 @@ const Footer = () => {
           a: { color: `text` },
         }}
       >
-        {isDark ? (
-          <img width="30" height="30" src="https://img.lekoarts.de/gatsby/logo_v2-light_w30.png" alt="LekoArts Logo" />
-        ) : (
-          <img width="30" height="30" src="https://img.lekoarts.de/gatsby/logo_v2_w30.png" alt="LekoArts Logo" />
-        )}
-        {` `}
+        Built with{` `}
+        <Link
+          aria-label="Link to the Gatsby's home page"
+          sx={{ ml: 2 }}
+          target="_blank"
+          href="https://gatsbyjs.com/"
+        >
+          Gatsby
+        </Link>
+        , inspired by{` `}
         <Link
           aria-label="Link to the theme's GitHub repository"
           sx={{ ml: 2 }}
+          target="_blank"
           href="https://github.com/LekoArts/gatsby-themes/tree/main/themes/gatsby-theme-cara"
-        >
-          Theme
-        </Link>
-        <div sx={{ mx: 1 }}>by</div>
-        {` `}
-        <Link
-          aria-label="Link to the theme author's website"
-          href="https://www.lekoarts.de?utm_source=cara&utm_medium=Theme"
         >
           LekoArts
         </Link>

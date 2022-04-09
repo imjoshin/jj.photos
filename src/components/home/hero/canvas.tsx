34af69aca30c7e9018d2e65ea5ae9a4900faae50
Josh <jcjohnson77@gmail.com>
2022-04-08 22:18:39 -0400

    Remove floor for now

--- src/components/home/hero/canvas.tsx
+++ src/components/home/hero/canvas.tsx
@@ -3,9 +3,7 @@ import { useEffect, useRef, useState } from "react"
 import { useFrame } from "react-three-fiber"
 import Camera from "./camera"
 import Portrait from "./portrait"
-import Floor from "./floor"
 import { PORTRAIT_POSITION_MODIFER } from "./const"
-
 
 // TODO use gatsby query images
 import Bird from "../../../images/florida_bird.jpg"
@@ -22,19 +20,24 @@ const HeroCanvas = () => {
     }
   })
 
+  // TODO there's some optimization to be done here
   const portraits = [...Array(5).keys()].reduce((acc, i) => {
     const position = -1 * PORTRAIT_POSITION_MODIFER.x * i - zPosRef.current
-    acc.push(<Portrait key={`${i}-left`} side="left" position={position} image={Bird}/>)
-    acc.push(<Portrait key={`${i}-right`} side="right" position={position} image={Bird}/>)
+
+    if (position < 0) {
+      acc.push(<Portrait key={`${i}-left`} side="left" position={position} image={Bird}/>)
+      acc.push(<Portrait key={`${i}-right`} side="right" position={position} image={Bird}/>)
+    }
+
     return acc
   }, [])
   
   return <group>
-      <Camera fov={90} near={0.1} far={1000} position={[0, 0.5, 0]} />
+      <Camera fov={90} near={0.1} far={10} position={[0, 0.5, 0]} />
       {/* <pointLight position={[10, 10, 10]} /> */}
       <ambientLight intensity={0.8} />
       {portraits}
-      <Floor />
+      {/* <Floor /> */}
   </group>
 }
 

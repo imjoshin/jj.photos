f338de5c760c46a1808b062131c88fe5097115ae
Josh <jcjohnson77@gmail.com>
2023-06-11 16:42:57 -0400

    Fix animation

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,10 +1,10 @@
-import * as React from "react"
-import { useRef } from "react"
-import { useEffect, useState } from "react"
-import { Icon } from "../../elements/icon"
-import { Nav } from "../../elements/nav"
-import { SEO } from "../../elements/seo"
-import * as styles from "./Home.module.css"
+import * as React from "react";
+import { useRef } from "react";
+import { useEffect, useState } from "react";
+import { Icon } from "../../elements/icon";
+import { Nav } from "../../elements/nav";
+import { SEO } from "../../elements/seo";
+import * as styles from "./Home.module.css";
 
 type HomeImage = {
   src: string,
@@ -12,126 +12,129 @@ type HomeImage = {
   accentColor: string,
   started?: number,
   className?: string,
-}
+};
 
 interface HomeProps {
   images: HomeImage[],
 }
 
-const ORIENTATION_RATIO_MIN = 4 / 3
+const ORIENTATION_RATIO_MIN = 4 / 3;
 
 const imageIsAllowedOnWindowRatio = (image: HomeImage) => {
   // window is not available in SSR, so just allow it
   if (typeof window === "undefined") {
-    return true
+    return true;
   }
 
-  const windowAspectRatio = window.innerWidth / window.innerHeight
+  const windowAspectRatio = window.innerWidth / window.innerHeight;
 
   // close to square, so allow all
   if (windowAspectRatio <= ORIENTATION_RATIO_MIN && windowAspectRatio >= 1 / ORIENTATION_RATIO_MIN) {
-    return true
+    return true;
   }
 
   // window is landscape and image is landscape
   if (windowAspectRatio > ORIENTATION_RATIO_MIN && image.aspectRatio > ORIENTATION_RATIO_MIN) {
-    return true
+    return true;
   }
 
   // window is portrait and image is portrait
   if (windowAspectRatio < 1 / ORIENTATION_RATIO_MIN && image.aspectRatio < 1 / ORIENTATION_RATIO_MIN) {
-    return true
+    return true;
   }
 
   // boo... not allowed. get outta here.
-  return false
-}
+  return false;
+};
 
 const getRandomImage = (images: HomeImage[], currentImage: HomeImage = null) => {
 
-  let random = images[Math.floor(Math.random() * images.length)]
+  let random = images[Math.floor(Math.random() * images.length)];
 
   while (!imageIsAllowedOnWindowRatio(random) || (currentImage && random.src === currentImage.src)) {
-    random = images[Math.floor(Math.random() * images.length)]
+    random = images[Math.floor(Math.random() * images.length)];
   }
 
   return {
     started: Date.now(),
     ...random,
-  }
-}
+  };
+};
 
-const ROTATE_INTERVAL = 10000
-const TRANSITION_INTERVAL = 1500
+const ROTATE_INTERVAL = 10000;
+const TRANSITION_INTERVAL = 1500;
 
 export const Home = ({ images }: HomeProps) => {
   // initialize background state
-  const [backgrounds, setBackgrounds] = useState([])
-  const backgroundsRef = useRef([])
-  backgroundsRef.current = backgrounds
+  const [backgrounds, setBackgrounds] = useState([]);
+  const backgroundsRef = useRef([]);
+  backgroundsRef.current = backgrounds;
 
-  const [accentColor, setAccentColor] = useState('#FFFFFF')
+  const [accentColor, setAccentColor] = useState('#FFFFFF');
 
   // kick off animation
   useEffect(() => {
     const initialBackgrounds = [
       getRandomImage(images),
-      getRandomImage(images),
+      {
+        ...getRandomImage(images),
+        className: styles.zoom,
+      },
     ];
     setBackgrounds(initialBackgrounds);
-    setAccentColor(initialBackgrounds[1].accentColor)
+    setAccentColor(initialBackgrounds[1].accentColor);
 
     const handleAnimations = (fadeOutLastImage = true) => {
-      const fadedBackgrounds = [...backgroundsRef.current]
-      fadedBackgrounds[fadedBackgrounds.length - 2].className = styles.zoom
+      const fadedBackgrounds = [...backgroundsRef.current];
+      fadedBackgrounds[fadedBackgrounds.length - 2].className = styles.zoom;
 
       if (fadeOutLastImage) {
-        fadedBackgrounds[fadedBackgrounds.length - 1].className = [styles.zoom, styles.fadeOut].join(' ')
+        fadedBackgrounds[fadedBackgrounds.length - 1].className = [styles.zoom, styles.fadeOut].join(' ');
       } else {
-        fadedBackgrounds[fadedBackgrounds.length - 1].className = styles.zoom
+        fadedBackgrounds[fadedBackgrounds.length - 1].className = styles.zoom;
       }
 
-      setBackgrounds(fadedBackgrounds)
-      setAccentColor(fadedBackgrounds[fadedBackgrounds.length - 2].accentColor)
-    }
+      setBackgrounds(fadedBackgrounds);
+      setAccentColor(fadedBackgrounds[fadedBackgrounds.length - 2].accentColor);
+    };
 
     // remove one image and add a new one every ROTATE_INTERVAL
     const handleImageLayers = () => {
-      const backgroundsMinusOne = backgroundsRef.current.slice(0, backgroundsRef.current.length - 1)
+      const backgroundsMinusOne = backgroundsRef.current.slice(0, backgroundsRef.current.length - 1);
       const newBackgrounds = [
         getRandomImage(
           images,
           backgroundsMinusOne[backgroundsMinusOne.length - 1]
-        ), 
+        ),
         ...backgroundsMinusOne
-      ]
+      ];
 
       // set the last image to fadeOut every TRANSITION_INTERVAL
-      setTimeout(handleAnimations, TRANSITION_INTERVAL)
-      setTimeout(handleImageLayers, ROTATE_INTERVAL)
-      setBackgrounds(newBackgrounds)
-    }
+      setTimeout(handleAnimations, TRANSITION_INTERVAL);
+      setTimeout(handleImageLayers, ROTATE_INTERVAL);
+      setBackgrounds(newBackgrounds);
+    };
 
     // TODO there's something weird here... images flicker on first load sometimes
     setTimeout(() => {
-      handleAnimations(false)
+      handleAnimations(false);
       setTimeout(() => {
-        handleAnimations()
-        setTimeout(handleImageLayers, ROTATE_INTERVAL)
-      }, TRANSITION_INTERVAL)
-    }, ROTATE_INTERVAL / 2)
+        handleAnimations();
+        setTimeout(handleImageLayers, ROTATE_INTERVAL);
+      }, TRANSITION_INTERVAL);
+    }, ROTATE_INTERVAL / 2);
 
     // TODO return cleanup for all these
-  }, [])
+  }, []);
 
   // order background divs
   const backgroundsDisplay = backgrounds.map(background => (
-    <div 
-      key={`${background.src}-${background.started}`} 
-      className={`${styles.backgroundImage} ${background.className || ''}`} 
-      style={{backgroundImage: `url(${background.src})`}} 
+    <div
+      key={`${background.src}-${background.started}`}
+      className={`${styles.backgroundImage} ${background.className || ''}`}
+      style={{ backgroundImage: `url(${background.src})` }}
     />
-  ))
+  ));
 
   return <div className={styles.home}>
     <SEO />
@@ -145,5 +148,5 @@ export const Home = ({ images }: HomeProps) => {
       </div>
       <Nav accentColor={accentColor} className={styles.nav} itemClassName={styles.navItem} />
     </div>
-  </div>
-}
+  </div>;
+};
